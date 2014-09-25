28ac39cfe7417d6c02a8e4ac37d8c55bf63dc98a
Anna Thomas <annat21@live.co.uk>
2014-09-25 12:40:54 +0100

    Moved over nodes from node-red-bluemix and created package.json file

---
+++ twilio/56-twilio-cf.js
@@ -0,0 +1,125 @@
+/**
+ * Copyright 2013 Andrew D Lindsay @AndrewDLindsay
+ * http://blog.thiseldo.co.uk
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+ 
+var cfEnv = require("cf-env");
+var cfCore = cfEnv.getCore();
+
+var services = [];
+if (cfCore.services['user-provided']) {
+    services = services.concat(cfCore.services['user-provided'].filter(function(v) {
+        return v.credentials.url == "https://api.twilio.com" && v.credentials.accountSID && v.credentials.authToken;
+    }).map(function(v) {
+        return {name:v.name,label:v.label};
+    }));
+}
+ 
+var RED = require(process.env.NODE_RED_HOME+"/red/red");
+var util = require('util');
+var twilio = require('twilio');
+
+var querystring = require('querystring');
+
+RED.httpAdmin.get('/twilio-api/vcap',function(req,res) {
+    res.send(JSON.stringify(services));
+});
+
+RED.httpAdmin.get('/twilio-api/:id',function(req,res) {
+    var credentials = RED.nodes.getCredentials(req.params.id);
+    if (credentials) {
+        res.send(JSON.stringify({hasToken:(credentials.token&&credentials.token!="")}));
+    } else {
+        res.send(JSON.stringify({}));
+    }
+});
+
+RED.httpAdmin.delete('/twilio-api/:id',function(req,res) {
+    RED.nodes.deleteCredentials(req.params.id);
+    res.send(200);
+});
+
+RED.httpAdmin.post('/twilio-api/:id',function(req,res) {
+    var body = "";
+    req.on('data', function(chunk) {
+        body+=chunk;
+    });
+    req.on('end', function(){
+        var newCreds = querystring.parse(body);
+        var credentials = RED.nodes.getCredentials(req.params.id)||{};
+        if (newCreds.token == "") {
+            delete credentials.token;
+        } else {
+            credentials.token = newCreds.token;
+        }
+        RED.nodes.addCredentials(req.params.id,credentials);
+        res.send(200);
+    });
+});
+
+function TwilioAPINode(n) {
+    RED.nodes.createNode(this,n);
+    this.sid = n.sid;
+    this.from = n.from;
+    this.name = n.name;
+    var credentials = RED.nodes.getCredentials(n.id);
+    if (credentials) {
+        this.token = credentials.token;
+    }
+}
+RED.nodes.registerType("twilio-api",TwilioAPINode);
+
+    
+function TwilioOutNode(n) {
+    RED.nodes.createNode(this,n);
+    this.number = n.number;
+    
+    if (n.service == "_ext_") {
+        this.api = RED.nodes.getNode(n.twilio);
+        this.twilioClient = twilio(this.api.sid,this.api.token);
+        this.fromNumber = this.api.from;
+    } else if (n.service != "") {
+        var twiliokey = cfEnv.getService(n.service);
+        if (twiliokey) {
+            this.twilioClient = twilio(twiliokey.credentials.accountSID, twiliokey.credentials.authToken);
+            this.fromNumber = n.from;
+        }
+    }
+    if (!this.twilioClient) {
+        this.error("missing twilio credentials");
+        return;
+    }
+    
+    var node = this;
+    this.on("input",function(msg) {
+        if (typeof(msg.payload) == 'object') {
+            msg.payload = JSON.stringify(msg.payload);
+        }
+        try {
+            // Send SMS
+            var tonum = node.number || msg.topic;
+            node.twilioClient.sendMessage( {to: tonum, from: node.fromNumber, body: msg.payload}, function(err, response) {
+                if (err) {
+                    node.error(err);
+                }
+                //console.log(response);
+            });
+        } catch (err) {
+            node.error(err);
+        }
+    });
+}
+RED.nodes.registerType("twilio out",TwilioOutNode);
---
+++ watson/smt.js
@@ -0,0 +1,97 @@
+/**
+ * Copyright 2013,2014 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+var http = require('http');
+var https = require('https');
+var url = require('url');
+
+// parse the VCAP_SERVICES env variable and get the http rest URI
+var services = JSON.parse(process.env.VCAP_SERVICES || "{}");
+var service = services["smtstg"] || "{}";
+
+var RED = require(process.env.NODE_RED_HOME + "/red/red");
+
+RED.httpAdmin.get('/translate/vcap', function(req, res) {
+    res.send(JSON.stringify(service));
+});
+
+module.exports = function(RED) {
+    function SMTNode(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+
+        if (service === "{}") {
+            this.on('input', function(msg) {
+                node.error("No machine translation service bound");
+            });
+        } else {
+            var cred = service[0]["credentials"];
+            var uri = url.parse(cred["uri"]);
+            var uid = cred["userid"];
+            var passwd = cred["password"];
+            var sids = cred["sids"];
+
+            this.on('input', function(msg) {
+                var sid = config.language || "";
+
+                // prepare HTTP request, input is stored in "msg.payload"
+                var rqt = "rt=text&sid=" + encodeURIComponent(sid) +
+                "&txt=" + encodeURIComponent(msg.payload);
+
+                var options = {
+                    hostname: uri.hostname,
+                    port: uri.port,
+                    path: uri.path,
+                    protocl: uri.protocol,
+                    method: 'POST',
+                    auth: uid + ":" + passwd,
+                    headers: {
+                        "Connection": "keep-alive",
+                        "Content-Type": "application/x-www-form-urlencoded",
+                        "Content-Length": rqt.length
+                    }
+                };
+
+                // issue http request
+                var httpclient = (uri.protocol=="https:" ? https : http);
+                var client = httpclient.request(options, function(resp) {
+                    resp.setEncoding('utf8');
+                    var rspbody = "";
+
+                    // we do the following to handle HTTP chunked response
+                    resp.on('data', function(chunk) {
+                        rspbody += chunk;
+                    });
+
+                    resp.on('end', function() {
+                        // output is stored in rspbody
+                        msg.payload = rspbody;
+                        node.send(msg);
+                    });
+                });
+
+                // handle http error
+                client.on('error', function(e) {
+                    node.error(e);
+                });
+
+                client.write(rqt);
+                client.end();
+            });
+        }
+    }
+    RED.nodes.registerType("translate",SMTNode);
+}
