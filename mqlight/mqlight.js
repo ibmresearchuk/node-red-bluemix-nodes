1d39e939b89624facbfb138eb7120b3becc3bf38
Nick O'Leary <nick.oleary@gmail.com>
2014-10-10 14:52:50 +0100

    Handle blank-vs-null mqlight share

--- mqlight/mqlight.js
+++ mqlight/mqlight.js
@@ -73,7 +73,8 @@ module.exports = function(RED) {
                         node.send(msg);
                     });
                     node.log("Subscribing to "+node.topic+(node.share?+" ["+node.share+"]":""));
-                    recvClient.subscribe(node.topic, function(err) {
+                    node.log("Node share: ["+node.share+"]");
+                    recvClient.subscribe(node.topic, node.share, function(err) {
                         if (err) {
                             node.error("Failed to subscribe: " + err);
                         } else {
